7a68fb186dd43fdf0d74362d72ae4d9315182198
neumoinvova-source <neumoinvova@gmail.com>
2026-09-08 17:37:11 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("🦉 OWL server работает!");
+});
+
+io.on("connection", (socket) => {
+
+    console.log("Пользователь подключился");
+
+    socket.on("send_message", (message) => {
+
+        io.emit("receive_message", message);
+
+    });
+
+    socket.on("disconnect", () => {
+
+        console.log("Пользователь отключился");
+
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(OWL server запущен на порту ${PORT});
+});
