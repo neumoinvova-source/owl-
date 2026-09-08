ace6c7a3ee376cfd8bfd6024b3b6ceef5b54121d
neumoinvova-source <neumoinvova@gmail.com>
2026-09-08 20:03:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({
+    origin: "https://neumoinvova-source.github.io",
+    methods: ["GET", "POST"]
+}));
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://neumoinvova-source.github.io",
         methods: ["GET", "POST"]
     }
 });
@@ -24,21 +27,17 @@ io.on("connection", (socket) => {
     console.log("Пользователь подключился");
 
     socket.on("send_message", (message) => {
-
         io.emit("receive_message", message);
-
     });
 
     socket.on("disconnect", () => {
-
         console.log("Пользователь отключился");
-
     });
 
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => { 
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`OWL server запущен на порту ${PORT}`);
 });
